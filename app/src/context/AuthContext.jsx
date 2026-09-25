d8fc7a5cfa9716180dbb2928dbcbc24b3f438b35
Balaji Laygude <balajiwork05@gmail.com>
2026-09-25 10:27:00 +0530

    context

---
+++ app/src/context/AuthContext.jsx
@@ -0,0 +1,69 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import api from "../services/api";
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const savedUser = localStorage.getItem("user");
+
+    if (savedUser) {
+      setUser(JSON.parse(savedUser));
+    }
+
+    setLoading(false);
+  }, []);
+
+  const login = (data) => {
+    console.log(data)
+    localStorage.setItem("token", data.token);
+    localStorage.setItem("user", JSON.stringify(data.user));
+    
+    setUser(data.user);
+  };
+  
+  const signup = (data) => {
+    console.log(data)
+    localStorage.setItem("token", data.token);
+    localStorage.setItem("user", JSON.stringify(data.user));
+
+    setUser(data.user);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+
+    setUser(null);
+  };
+
+  const changePassword = async (password) => {
+    const response = await api.put("/auth/password", {
+      password,
+    });
+
+    return response.data;
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        loading,
+        login,
+        signup,
+        logout,
+        changePassword,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
